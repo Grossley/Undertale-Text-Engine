a2eff8bba6aec5b0e4e7bd81e822f7f80ba3cbfc
Grossley <42724744+Grossley@users.noreply.github.com>
2021-12-27 15:00:04 -0800

    Add rudimentary web server

---
+++ Start.bat
@@ -0,0 +1,5 @@
+@echo off
+rem Comment out the next line (using "rem") to not clear save data on start
+rem del "%AppData%\Macromedia\Flash Player\#SharedObjects\TZRN6Q94\#localhost\plantsvszombies.swf\pvz.sol"
+start StartWebServer.bat
+start index.html
---
+++ StartWebServer.bat
@@ -0,0 +1,2 @@
+@echo off
+"php-7.4.0-Win32-vc15-x64\php.exe" -S localhost:8080
